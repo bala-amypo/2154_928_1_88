f38ccc9558175000708f7c170364d552bd545181
bala-amypo <bala@amypo.in>
2025-12-19 10:29:51 +0000

    User commit

--- src/main/java/com/example/demo/service/BookingLogService.java
+++ src/main/java/com/example/demo/service/BookingLogService.java
@@ -1,8 +1,11 @@
 package com.example.demo.service;
 
-import com.example.demo.entity.BookingLog;
 import java.util.List;
+import com.example.demo.entity.BookingLog;
 
 public interface BookingLogService {
+
+    BookingLog saveLog(BookingLog log);
+
     List<BookingLog> findByBookingId(Long bookingId);
 }
--- src/main/java/com/example/demo/service/BookingLogServiceImpl.java
+++ src/main/java/com/example/demo/service/BookingLogServiceImpl.java
@@ -1,21 +1,28 @@
-package com.example.demo.service.impl;
+package com.example.demo.service;
+
+import java.util.List;
 
 import org.springframework.stereotype.Service;
-import java.util.List;
-import com.example.demo.service.BookingLogService;
+
 import com.example.demo.entity.BookingLog;
 import com.example.demo.repository.BookingLogRepository;
 
 @Service
 public class BookingLogServiceImpl implements BookingLogService {
 
-    private final BookingLogRepository repo;
+    private final BookingLogRepository repository;
 
-    public BookingLogServiceImpl(BookingLogRepository repo) {
-        this.repo = repo;
+    public BookingLogServiceImpl(BookingLogRepository repository) {
+        this.repository = repository;
     }
 
+    @Override
+    public BookingLog saveLog(BookingLog log) {
+        return repository.save(log);
+    }
+
+    @Override
     public List<BookingLog> findByBookingId(Long bookingId) {
-        return repo.findAll();
+        return repository.findByBookingId(bookingId);
     }
 }
