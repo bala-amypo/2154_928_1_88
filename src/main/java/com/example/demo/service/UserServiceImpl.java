6e8d8e8f6d241adf9da47536c7595429ea25363c
bala-amypo <bala@amypo.in>
2025-12-24 07:52:17 +0000

    User commit

--- src/main/java/com/example/demo/service/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.example.demo.service.impl;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.demo.exception.BadRequestException;
 import com.example.demo.model.User;
@@ -8,6 +9,7 @@ import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserService;
 
 @Service
+@Transactional
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
@@ -25,21 +27,17 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional(readOnly = true)
     public User findByEmail(String email) {
         return userRepository.findByEmail(email)
                 .orElseThrow(() -> new BadRequestException("User not found"));
     }
 
-    
     @Override
+    @Transactional(readOnly = true)
     public User login(String email, String password) {
-        User user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new BadRequestException("Invalid email"));
 
-        if (!user.getPassword().equals(password)) {
-            throw new BadRequestException("Invalid password");
-        }
-
-        return user;
+        return userRepository.findByEmailAndPassword(email, password)
+                .orElseThrow(() -> new BadRequestException("Invalid email or password"));
     }
 }
