1e6ddb842f6fc2de294263959347893fb45ed803
bala-amypo <bala@amypo.in>
2025-12-26 06:08:03 +0000

    User commit

--- src/main/java/com/example/demo/dto/RegisterRequest.java
+++ src/main/java/com/example/demo/dto/RegisterRequest.java
@@ -1,26 +1,25 @@
 package com.example.demo.dto;
 
-public class RegisterRequest {
+import java.time.LocalDateTime;
 
-    private String name;
-    private String email;
-    private String password;
+public class BookingRequest {
+    private Long userId;
+    private Long facilityId;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
 
-    public RegisterRequest() {}
-
-    public RegisterRequest(String name, String email, String password){
-        this.name = name;
-        this.email = email;
-        this.password = password;
-    }
+    public BookingRequest() {}
 
     // getters & setters
-    public String getName(){ return name; }
-    public void setName(String name){ this.name = name; }
+    public Long getUserId(){ return userId; }
+    public void setUserId(Long userId){ this.userId = userId; }
 
-    public String getEmail(){ return email; }
-    public void setEmail(String email){ this.email = email; }
+    public Long getFacilityId(){ return facilityId; }
+    public void setFacilityId(Long facilityId){ this.facilityId = facilityId; }
 
-    public String getPassword(){ return password; }
-    public void setPassword(String password){ this.password = password; }
+    public LocalDateTime getStartTime(){ return startTime; }
+    public void setStartTime(LocalDateTime startTime){ this.startTime = startTime; }
+
+    public LocalDateTime getEndTime(){ return endTime; }
+    public void setEndTime(LocalDateTime endTime){ this.endTime = endTime; }
 }
